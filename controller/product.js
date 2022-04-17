7b0bb5afe06afd4eda3160f2457e94fb10c4d428
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2022-04-17 21:30:52 +0500

    Corrected Add Product
    It shows that i changed alot, but its only due to prettier

--- controller/product.js
+++ controller/product.js
@@ -1,73 +1,69 @@
 import Product from "../models/Product.js";
 
 //to get single product
-export const getProduct = async (req,res) =>{
-   const { id} = req.params
-    try{
-        
-        
-        const product = await Product.findById(id);
- 
-        res.json(product);
-    }
-    catch(err){
-        res.json({message: err});
-    }
-}
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
 
-//to get all products 
-export const getProducts = async (req,res) =>{
-    try{
-        const product = await Product.find();
-        res.json(product);
-    } catch(err){
-        res.json({message: err});
-    }
-}
+    res.json(product);
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
+
+//to get all products
+export const getProducts = async (req, res) => {
+  try {
+    const product = await Product.find();
+    res.json(product);
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
 
 //to create product
-export const createProduct = async (req,res) =>{
- 
-    const {title, desc, price} = req.body;
-    console.log(title,desc,price);
-    const product = new Product({
-        productname: title,
-        desc: desc,
-        productcost: price
-    });
-   
-    try{
-        const savedProduct = await product.save()
-        res.json(savedProduct);
-    }
-    catch(err) {
-        res.json({message: err});
-    }
-}
+export const createProduct = async (req, res) => {
+  const { title, desc, price } = req.body;
+  console.log(title, desc, price);
+  const product = new Product({
+    productname: title,
+    desc: desc,
+    productcost: price,
+  });
+
+  try {
+    const savedProduct = await product.save();
+    res.json(savedProduct);
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
 
 //to update product
-export const updateProduct = async (req,res) =>{
-     console.log("We are here")
-    const {id} = req.params;
-    const {title, desc, price} = req.body;
-    console.log(title,desc,price);
+export const updateProduct = async (req, res) => {
+  console.log("We are here");
+  const { id } = req.params;
+  const { title, desc, price } = req.body;
+  console.log(title, desc, price);
 
-    try{
-    const updatedProduct = await Product.updateOne({ id },
-        {$set: {productname: title,
-            desc: desc,
-            productcost: price }});
-    res.json(updatedProduct);
-} catch(err){
-    res.json({message: err});
-}}
+  try {
+    const updatedProduct = await Product.updateOne(
+      { id },
+      { $set: { productname: title, desc: desc, productcost: price } }
+    );
+    res.json({Message : "Product Updated Successfully"});
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
 
 //to delete product
-export const deleteProduct = async (req,res) =>{
-    try{
-        const removedProduct = await Product.deleteOne({ _id: req.params.productID});
-        res.json(removedProduct);
-    } catch(err){
-        res.json({message: err});
-    }
-}
+export const deleteProduct = async (req, res) => {
+  try {
+    const removedProduct = await Product.deleteOne({ _id: req.params.id });
+    res.json({Message : "Product Deleted Successfully"});
+  } catch (err) {
+    res.json({ message: err });
+  }
+};
--- models/Product.js
+++ models/Product.js
@@ -1,27 +1,25 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema(
-    {
-      productname: {
-        type: String,
-        require: true,
-        min: 3,
-        max: 20,
-        unique: true,
-      },
-      desc: {
-        type: String,
-        max: 50,
-      },
-      productcost: {
-        type: String,
-        max: 50,
-      },
-      
-     
+  {
+    productname: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
     },
-    { timestamps: true }
-  );
-  
-const product =  mongoose.model("Product", ProductSchema);
-export default product
+    desc: {
+      type: String,
+      max: 50,
+    },
+    productcost: {
+      type: String,
+      max: 50,
+    },
+  },
+  { timestamps: true }
+);
+
+const product = mongoose.model("Product", ProductSchema);
+export default product;
